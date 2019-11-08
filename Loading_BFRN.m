7eb01c0bb38f1a533b24bd9a0cb136561e41f341
jkingslake <j.kingslake@columbia.edu>
2019-11-08 10:25:06 -0500

    No changes

--- Loading_BFRN.m
+++ Loading_BFRN.m
@@ -1,10 +1,11 @@
 %% Script to load buttressing reponse number results from Reese et al., 2016 
 
 %% 1. Load buttressing response number output
-load('ButtressingFluxResponseNumbers')
+load('BFRN/ButtressingFluxResponseNumbers')
 
 B = GRIDobj(x,y,BFRN);
 
+% imagesc(B)
 
 
 
