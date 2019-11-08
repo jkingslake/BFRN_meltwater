d748b82cb2692ad36a01166684d022a27a83f9c1
jkingslake <j.kingslake@columbia.edu>
2019-11-08 10:24:07 -0500

    Added latest BFRNs from Ronja

---
+++ Loading_FutureMeltData.m
@@ -0,0 +1,8 @@
+%% script for loading melt data from Trusel et al,, 2015
+
+
+
+
+%% 1. Load melt data
+
+M = GRIDobj('Truseletal2015_2091-2100_Melt\tas_CLM4_ensembleMean_rcp85_Mar2015_maskedBiasCorrectedProjectedMeltFlux_Mar2015_2091-2100Mean_rectified_cutline_lowMeltMasked.tif');
